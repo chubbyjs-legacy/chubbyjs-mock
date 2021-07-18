402d3ea27d5ac27229fc37fd378d5eb0209464b4
Dominik Zogg <dominik.zogg@hotmail.com>
2021-07-18 16:56:48 +0200

    update test

--- tests/MockByCalls.test.ts
+++ tests/MockByCalls.test.ts
@@ -1,7 +1,8 @@
 import Call from '../src/Call';
 import MockByCalls from '../src/MockByCalls';
+import { expect, test } from '@jest/globals';
 
-test('factory', () => {
+test('mocked methods', () => {
     class Sample {
         public a(name: string): string {
             return name;
@@ -12,7 +13,11 @@ test('factory', () => {
         }
     }
 
-    const sample = MockByCalls<Sample>([Call.create('a').with('name1').willReturn('name1')]);
+    const sample = MockByCalls<Sample>([
+        Call.create('a').with('name1').willReturn('name1'),
+        Call.create('b').with('name2').willReturn('name2'),
+    ]);
 
     expect(sample.a('name1')).toBe('name1');
+    expect(sample.b('name2')).toBe('name2');
 });
